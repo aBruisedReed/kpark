45f9f3b39b5dbd01dac354d4906e8064a1fd0881
younjiwon <younjiwon@localhost>
2020-10-28 20:23:22 +0900

    main

--- kpark/src/main/java/com/kgit/kpark/HomeController.java
+++ kpark/src/main/java/com/kgit/kpark/HomeController.java
@@ -4,6 +4,9 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -11,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -24,17 +28,11 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	private ModelAndView home(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String viewName = "home";
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName(viewName);
+		return mav;
 	}
 	
 	@RequestMapping(value="common/{pageName}")
