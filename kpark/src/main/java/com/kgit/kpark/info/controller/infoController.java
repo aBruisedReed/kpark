20e8fb907cc61e7831f50fef694fc1b1132ccb84
Administrator <Administrator@DESKTOP-0QGB3UT>
2020-11-02 21:13:15 +0900

    인포 컨트롤러 연결

--- kpark/src/main/java/com/kgit/kpark/info/controller/infoController.java
+++ kpark/src/main/java/com/kgit/kpark/info/controller/infoController.java
@@ -1,5 +1,11 @@
 package com.kgit.kpark.info.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+
 public interface infoController {
-	
+	public ModelAndView brandInfo(HttpServletRequest request, HttpServletResponse response)throws Exception;
+	public ModelAndView brandList(HttpServletRequest request, HttpServletResponse response)throws Exception;
 }
--- kpark/src/main/java/com/kgit/kpark/info/controller/infoControllerImpl.java
+++ kpark/src/main/java/com/kgit/kpark/info/controller/infoControllerImpl.java
@@ -8,14 +8,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@Controller("infoController")
 @RequestMapping(value = "/info/*")
 public class infoControllerImpl implements infoController {
-	@RequestMapping(value = {"/info/*"}, method = RequestMethod.GET)
-	private ModelAndView adminSelect(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+	@Override
+	@RequestMapping(value = "/info/brandInfo.do", method = RequestMethod.POST)
+	public ModelAndView brandInfo(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String viewName = (String)request.getAttribute("viewName");
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName(viewName);
 		return mav;
 	}
+
+	@Override
+	@RequestMapping(value = "/info/brandList.do", method = RequestMethod.POST)
+	public ModelAndView brandList(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String viewName = (String)request.getAttribute("viewName");
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName(viewName);
+		return mav;
+	}
+//	@RequestMapping(value = {"/info/*"}, method = RequestMethod.GET)
+//	private ModelAndView adminSelect(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		String viewName = (String)request.getAttribute("viewName");
+//		ModelAndView mav = new ModelAndView();
+//		mav.setViewName(viewName);
+//		return mav;
+//	}
 }
