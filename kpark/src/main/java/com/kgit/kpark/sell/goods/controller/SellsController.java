784f8048aed45d15650d0c0171a571359e6bfae9
filla <filla@LAPTOP-SNVMPB0I>
2020-11-11 18:05:34 +0900

    판매상담 페이지 로그인 세션제외한 나머지 DB연결

---
+++ kpark/src/main/java/com/kgit/kpark/sell/goods/controller/SellsController.java
@@ -0,0 +1,13 @@
+package com.kgit.kpark.sell.goods.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import com.kgit.kpark.sell.goods.vo.SellVO;
+import com.kgit.kpark.member.vo.MemberVO;
+
+public interface SellsController {
+	public ModelAndView addSell(SellVO sellVO, HttpServletRequest request, HttpServletResponse response) throws Exception;
+}
---
+++ kpark/src/main/java/com/kgit/kpark/sell/goods/controller/SellsControllerImpl.java
@@ -0,0 +1,49 @@
+package com.kgit.kpark.sell.goods.controller;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.kgit.kpark.sell.goods.controller.SellsController;
+import com.kgit.kpark.sell.goods.service.SellService;
+import com.kgit.kpark.sell.goods.vo.SellVO;
+
+@Controller("sellsController")
+@EnableAspectJAutoProxy
+public class SellsControllerImpl implements SellsController {
+	private static final Logger logger = LoggerFactory.getLogger(SellsControllerImpl.class);
+	
+	@Autowired 
+	SellService sellService;
+	@Autowired
+	SellVO sellVO;
+	
+	@Override
+	@RequestMapping(value = "sell/addSell.do", method = RequestMethod.POST)
+	public ModelAndView addSell(SellVO sellVO, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		int result = sellService.addSell(sellVO);
+		//ModelAndView mav = new ModelAndView("redirect:/admin/adminSelect.do");
+		//out.print("<script> alert('판매신청을 완료하었습니다..'); location.href='/kpark/member/login.do'; </script>");
+		//return mav;
+		System.out.println(sellVO.getCarModel());
+		out.print("<script> alert('판매신청을 완료하었습니다..'); location.href='/kpark/main.do'; </script>");
+		out.flush();
+		return null;
+	}
+
+
+
+}
